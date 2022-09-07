da8525450a0ee8b6c9789f5f279f1ad944cb4889
Alexander Sadovsky <alino.sadovsky@gmail.com>
2022-09-07 12:40:52 +0200

    handle token expiration with automaticSilentRenew

--- bg-auth.js
+++ bg-auth.js
@@ -9,16 +9,12 @@ const settings = {
     redirect_uri: redirectUri,
     post_logout_redirect_uri: redirectUri,
 
-    // loadUserInfo: true,
     response_type: "code",
     response_mode: "fragment",
     // scope: "openid email roles",
 
-    // popup_redirect_uri: url + "/sample-popup-signin.html",
-    // popup_post_logout_redirect_uri: url + "/sample-popup-signout.html",
-
-    // silent_redirect_uri: url + "/sample-silent.html",
-    // automaticSilentRenew: true,
+    silent_redirect_uri: redirectUri,
+    automaticSilentRenew: true,
     //silentRequestTimeout: 10000,
 
     filterProtocolClaims: true
